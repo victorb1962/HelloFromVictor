fabd2c03303fb9d5c0a67a8464c1bcec8af7a87b
VictorB <victorb62@byui.edu>
2021-01-29 12:54:21 -0600

    Initial commit

---
+++ src/w04hello/Main.java
@@ -0,0 +1,9 @@
+package w04hello;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("Hello, my name is Victor Boxall.");
+    }
+}
