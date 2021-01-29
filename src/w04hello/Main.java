f13bbdfbb7d31d420f9b7ce30db3e8a3e7dbe7b6
olusegab <oabayomi24@gmail.com>
2021-01-29 22:47:25 +0100

    Hello from Yomi to Victor

--- src/w04hello/Main.java
+++ src/w04hello/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.print("Hello, my name is Victor Boxall.");
+        System.out.print("Hello Victor Boxall, my name is Olusegun Abayomi.");
     }
 }
